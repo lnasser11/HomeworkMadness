27a16d3b17fe20d8d2965a680b7fa1d05344f15d
lnasser11 <luccanasser11@gmail.com>
2022-05-18 20:19:21 -0300

    Update game.py
    Criei as bases das classes que queremos usar no projeto, mas não preenchi pois vamos dividir o que cada um irá fazer e vamos ir preenchendo conforme os André desenha os sprites e o David faz os sons

--- game.py
+++ game.py
@@ -18,15 +18,79 @@ clock = pygame.time.Clock()
 #Classes
 
 class Game():
-    '''Classe para controlar a gameplay do jogo'''
+    #Classe para controlar a gameplay do jogo 
     def __init__(self):
         pass
 
     def update(self):
-        '''Classe para atualizar o objeto'''
+        #Classe para atualizar o objeto 
+        pass
+
+    def draw(self):
+        #desenha as partes da tela 
+        pass
+
+    def colisao(self):
+        #checa as colisoes dos monstros e do jogador 
+        pass
+
+    def new_round(self):
+        #spawna os monstros 
+
+        pass
+
+    def inimigo(self):
+        #escolhe um novo monstro para ser o alvo
+        pass
+
+    def pause(self):
+        #pausa o jogo
+        pass
+
+    def restart(self):
+        #reseta o jogo
         pass
 
 
+class Player(pygame.sprite.Sprite):
+    #Classe que o jogador controla
+    def __init__(self):
+        #inicializa o jogador
+        pass
+
+    def update(self):
+        #atualiza o jogador
+        pass
+
+    def tp(self):
+        #teletransporta o jogador para a safe zone
+        pass
+
+    def reseta(self):
+        #reseta a posição do jogador
+        pass
+
+class Inimigo(pygame.sprite.Sprite):
+    #classe para criar os inimigos
+    def __init__(self):
+        #inicializa os monstros
+        pass
+
+    def update(self):
+        #atualiza o monstro
+        pass
+
+
+#Criação do jogador
+player_group = pygame.sprite.Group()
+player = Player()
+player_group.add(player)
+
+#Criação dos monstros
+monster_group = pygame.sprite.Group()
+
+#Game object
+game_obj = Game()
 #Game loop
 game = True
 
@@ -36,6 +100,20 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+
+    #tela do jogo
+    display.fill((0,0,0))
+
+    #Atualização dos sprites
+    player_group.update()
+    player_group.draw(display)
+
+    monster_group.update()
+    monster_group.draw(display)
+
+    game_obj.update()
+    game_obj.draw()
+    
     #Atualiza a tela do jogo
     pygame.display.update()
     clock.tick(FPS)
