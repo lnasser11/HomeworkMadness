f3ef29e8c10b464e2be3282f8440c659b517397e
lnasser11 <luccanasser11@gmail.com>
2022-05-24 01:27:56 -0300

    update
    Coloquei as imagens dos professore para entregar as lições e fiz mais algumas funções nas classes

--- game.py
+++ game.py
@@ -9,8 +9,11 @@ HEIGHT = 700
 
 #Criação da janela
 display = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Teste")
+pygame.display.set_caption("Homework Madness")
 
+#fundo do jogo
+bg = pygame.image.load('deco_bg.jpg')
+bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
 #FPS e Velocidade do jogo
 FPS = 60
 clock = pygame.time.Clock() 
@@ -19,17 +22,72 @@ clock = pygame.time.Clock()
 
 class Game():
     #Classe para controlar a gameplay do jogo 
-    def __init__(self):
-        pass
+    def __init__(self, player, grupo_inimigo):
+        self.pontos = 0
+        self.round = 0
+
+        self.round_time = 0
+        self.frame_count = 0
+
+        self.player = player
+        self.grp_ini = grupo_inimigo
+        
+        self.prox_level = pygame.mixer.Sound('testesom.wav')
+
+        self.fonte = pygame.font.Font('Arial', 24)
+
+        #professores
+        areia = pygame.image.load('areia.jpg')
+        areia = pygame.transform.scale(areia, (64,64))
+        builder = pygame.image.load('builder.jpg')
+        builder = pygame.transform.scale(builder, (64,64))
+        ffjr = pygame.image.load('ffjr.jpg')
+        ffjr = pygame.transform.scale(ffjr, (64,64))
+        mural = pygame.image.load('mural.jpg')
+        mural = pygame.transform.scale(mural, (64,64))
+
+        #Prof que precisa entregar a lição de casa
+        self.alvos = [areia, builder, ffjr, mural]
+
+        self.alvo_escolha = random.randint(0,3)
+        self.alvo = self.alvos[self.alvo_escolha]
+
+        self.alvo_rect = self.alvo.get_rect()
+        self.alvo_rect.centerx = WIDTH//2
+        self.alvo_rect.top = 30
+
 
     def update(self):
-        #Classe para atualizar o objeto 
-        pass
-
+        self.round_time += 1
+        
+        self.colisao()
     def draw(self):
         #desenha as partes da tela 
-        pass
+        WHITE = (255,255,255)
+        BLUE = (20,176,235)
+        PURPLE = (226,73,243)
+        GREEN = (87,201,47)
+        YELLOW = (243,157,20)
 
+        cores = [BLUE,PURPLE,GREEN,YELLOW]
+
+        txt_entrega = self.font.render('Entrega atual:', True, WHITE)
+        entrega_rect = txt_entrega.get_rect()
+        entrega_rect.centerx = WIDTH//2
+        entrega_rect.top = 5
+
+        txt_pontos = self.font.render('Pontos: ' + str(self.pontos), True, WHITE)
+        pontos_rect = txt_pontos.get_rect()
+        pontos_rect.topleft = (5,5)
+
+        txt_vidas = self.font.render('Vidas: ' + str(self.player.lives), True, WHITE)
+        vidas_rect = txt_vidas.get_rect()
+        vidas_rect.topleft = (5,35)
+
+        txt_rounds = self.font.render('Round atual: ' + str(self.round), True, WHITE)
+        rounds_rect = txt_rounds.get_rect()
+        rounds_rect.topleft = (5,65)
+        
     def colisao(self):
         #checa as colisoes dos monstros e do jogador 
         pass
@@ -106,7 +164,7 @@ class Inimigo(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
         
-        # Professor --> decide o tipo do professor que vai spawnar atraves de um numero int
+        # Professor --> decide o tipo do professor que vai spawnar atraves da lista de profs 
         self.type = professor
 
         # Caminho aleatorio dos professores
@@ -131,7 +189,7 @@ player_group.add(player)
 
 #Criação dos monstros
 monster_group = pygame.sprite.Group()
-#testeso
+#testes
 m1 = pygame.image.load('monstro4_ph.png')
 m1 = pygame.transform.scale(m1, (64, 64))
 monstro = Inimigo(500, 500, m1, 1)
@@ -142,7 +200,7 @@ monstro = Inimigo(100, 500, m2, 0)
 monster_group.add(monstro)
 
 #Game object
-game_obj = Game()
+game_obj = Game(player, monster_group)
 #Game loop
 game = True
 
@@ -155,6 +213,7 @@ while game:
 
     #tela do jogo
     display.fill((0,0,0))
+    display.blit(bg, (0,0))
 
     #Atualização dos sprites
     player_group.update()
