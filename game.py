2921544055d772773c253ab4b3a242ee0a1f753a
lnasser11 <luccanasser11@gmail.com>
2022-06-08 00:42:04 -0300

    finalização

--- game.py
+++ game.py
@@ -14,6 +14,13 @@ pygame.display.set_caption("Homework Madness")
 #fundo do jogo
 bg = pygame.image.load('assets/bg.png')
 bg = pygame.transform.scale(bg, (WIDTH, HEIGHT))
+
+inicio = pygame.image.load('assets/inicio.png')
+inicio = pygame.transform.scale(inicio, (WIDTH, HEIGHT))
+display.blit(inicio, (0,0))
+fim = pygame.image.load('assets/fim.png')
+fim = pygame.transform.scale(fim, (WIDTH, HEIGHT))
+
 #FPS e Velocidade do jogo
 FPS = 60
 clock = pygame.time.Clock() 
@@ -32,18 +39,18 @@ class Game():
         self.player = player
         self.grp_ini = grupo_inimigo
         
-        self.prox_level = pygame.mixer.Sound('testesom.wav')
+        self.prox_level = pygame.mixer.Sound('assets/passadenivel.wav')
 
-        self.fonte = pygame.font.Font('fonte.ttf', 24)
-
+        self.fonte = pygame.font.Font('assets/fonte.ttf', 24)
+        
         #professores
-        areia = pygame.image.load('areia.jpg')
+        areia = pygame.image.load('assets/areia.jpg')
         areia = pygame.transform.scale(areia, (64,64))
-        builder = pygame.image.load('builder.jpg')
+        builder = pygame.image.load('assets/builder.jpg')
         builder = pygame.transform.scale(builder, (64,64))
-        ffjr = pygame.image.load('ffjr.jpg')
+        ffjr = pygame.image.load('assets/ffjr.jpg')
         ffjr = pygame.transform.scale(ffjr, (64,64))
-        mural = pygame.image.load('mural.jpg')
+        mural = pygame.image.load('assets/mural.jpg')
         mural = pygame.transform.scale(mural, (64,64))
 
         #Prof que precisa entregar a lição de casa
@@ -70,7 +77,7 @@ class Game():
         ROXO = (129, 50, 168)
         VERDE = (50, 168, 70)
         LARANJA = (201, 126, 34)
-
+        
         cores = [AZUL,ROXO,VERDE,LARANJA]
 
         txt_entrega = self.fonte.render('Entrega atual:', True, WHITE)
@@ -127,7 +134,9 @@ class Game():
                 self.player.som_morte.play()
                 self.player.vidas -= 1
                 if self.player.vidas <= 0:
+                    display.blit(fim, (0,0))
                     self.pause("Pontuação: " + str(self.pontos), "Aperte 'ENTER' para jogar novamente!")
+
                     self.restart()
                 self.player.reset()
 
@@ -144,6 +153,7 @@ class Game():
         for prof in self.grp_ini:
             self.grp_ini.remove(prof)
         
+        #fix
         for i in range(self.round):
             self.grp_ini.add(Inimigo(random.randint(11, WIDTH - 75), random.randint(155, HEIGHT-394), self.alvos[0], 0))
             self.grp_ini.add(Inimigo(random.randint(11, WIDTH - 75), random.randint(155, HEIGHT-394), self.alvos[1], 1))
@@ -172,7 +182,6 @@ class Game():
         inho_rect = textinho.get_rect()
         inho_rect.center = (WIDTH//2, HEIGHT//2 + 64)
 
-        display.fill((0,0,0))
         display.blit(textao, textao_rect)
         display.blit(textinho, inho_rect)
         pygame.display.update()
@@ -213,23 +222,26 @@ class Player(pygame.sprite.Sprite):
         self.warps = 2
         self.velocidade = 8
 
-        self.som_entrega = pygame.mixer.Sound('testesom.wav')
-        self.som_morte = pygame.mixer.Sound('testesom.wav')
-        self.som_warp = pygame.mixer.Sound('testesom.wav')
-        self.som_sem_warp = pygame.mixer.Sound('testesom.wav')
+        self.som_entrega = pygame.mixer.Sound('assets/ponto.wav')
+        self.som_morte = pygame.mixer.Sound('assets/dano.wav')
+        self.som_warp = pygame.mixer.Sound('assets/tp.wav')
+        self.som_sem_warp = pygame.mixer.Sound('assets/semtp.wav')
 
     def update(self):
         #atualiza o jogador
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_LEFT] and self.rect.left > 23:
+        
+
+        if keys[pygame.K_LEFT] and (self.rect.left > 23):
             self.rect.x -= self.velocidade
-        if keys[pygame.K_RIGHT] and self.rect.right < WIDTH-23:
+        if keys[pygame.K_RIGHT] and self.rect.right <= WIDTH-23:
             self.rect.x += self.velocidade
         if keys[pygame.K_UP] and self.rect.top > 150:
             self.rect.y -= self.velocidade
         if keys[pygame.K_DOWN] and self.rect.bottom < HEIGHT - 185:
             self.rect.y += self.velocidade
+
 
     def tp(self):
         #teletransporta o jogador para a safe zone (corredor)
@@ -242,7 +254,7 @@ class Player(pygame.sprite.Sprite):
         #reseta a posição do jogador
         self.rect.centerx = WIDTH//2
         self.rect.bottom = HEIGHT - 23
-    
+
 class Inimigo(pygame.sprite.Sprite):
     #classe para criar os inimigos
     def __init__(self, x, y, image, professor):
@@ -257,7 +269,7 @@ class Inimigo(pygame.sprite.Sprite):
 
         # Caminho aleatorio dos professores
         self.dx = random.choice([-1,1])
-        self.dy = random.choice([-1,1]) # -1 vai para a esquerda e 1 para a direita
+        self.dy = random.choice([-1,1,]) # -1 vai para a esquerda e 1 para a direita
         self.velocidade = random.randint(1,5)
 
     def update(self):
@@ -280,7 +292,7 @@ monster_group = pygame.sprite.Group()
 
 #Game object
 game_obj = Game(player, monster_group)
-game_obj.pause('Homework Madness', "Aperte 'ENTER' para começar")
+game_obj.pause('Você é um Fantasma! Sua última missão é entregar sua lição para seus antigos professores!', "Use as SETAS para se mover e ESPAÇO para sair da sala! Aperte 'ENTER' para começar!")
 game_obj.new_round()
 
 #Game loop
